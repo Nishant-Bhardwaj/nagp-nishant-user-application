cd43f716749e48b60a78ce2f300ef808f1a0ec62
Nishant Bhardwaj <52878003+Nishant-Bhardwaj@users.noreply.github.com>
2023-05-14 11:10:21 +0530

    cicd

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,5 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build:{
-    lib:{
-      entry: './src/main.ts',
-      name: 'user-application'
-    }
-  },
   plugins: [svelte()],
 })
